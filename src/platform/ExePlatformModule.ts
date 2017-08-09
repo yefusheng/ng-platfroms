9eaed9f657910864f6ae0044adde05dffabdecc6
yefusheng <372758022@qq.com>
2017-08-09 17:41:25 +0800

    代码优化

--- src/platform/ExePlatformModule.ts
+++ src/platform/ExePlatformModule.ts
@@ -3,23 +3,18 @@ import {NgModule} from '@angular/core';
 import { ExePlatformService} from "./ExePlatform.service";
 import {ComponentsFactoryService} from "./component/ComponentsFactory.service";
 import {exeUploadFileComponent} from "./component/fileUpload/exeUploadFile.component";
-
-import {exeUploadFileNativeComponent} from "./component/fileUpload/native/UploadFileNative.component";
-import {exeUploadFilePcComponent} from "./component/fileUpload/pc/UploadFilePc.component";
-
-import {exeUploadFileWechatComponent} from "./component/fileUpload/wechat/UploadFileWechat.component";
 import {BaseAuthService} from "./BaseAuth.service";
 import {AuthService} from "../service/Auth.service";
 import {PlatformStragety} from "./PlatformStragety";
-
+//导入对应的平台模块
 let platformModule=new PlatformStragety().getStragety();
 @NgModule({
   imports: [
     platformModule,
   ],
   exports: [exeUploadFileComponent],
-  declarations: [exeUploadFileComponent,exeUploadFileWechatComponent],
-  entryComponents: [exeUploadFileComponent,exeUploadFileWechatComponent],
+  declarations: [exeUploadFileComponent],
+  entryComponents: [exeUploadFileComponent],
   providers: [
 
     {
