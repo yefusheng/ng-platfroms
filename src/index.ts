da3e58c437f3aaaca4a4579cf323756ce460c3e2
yefusheng <372758022@qq.com>
2017-08-24 17:51:29 +0800

    --index

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,14 @@ export {ExePlatformModule} from './ExePlatformModule';
 export {AuthService} from './Auth.service';
 export {StorageService} from './service/Storage.service';
 export {LoggerService} from './service/Logger.service';
+export {TransFileService} from './service/TransFile.service'
 export {ExePlatformService} from './ExePlatform.service';
 
 //export {ExePlatformConponentModule} from './dynamic-component/ExePlatformConponentModule';/
 export {WindowsPlatformModule} from './windows/WindowsPlatformModule';
 export {WechatPlatformModule} from './Wechat/WechatPlatformModule'
 export {NativePlatformModule} from './Native/NativePlatformModule'
+
+export {NativeService} from './native/service/transfer/NativeService'
+
+export {log} from './util/util'
