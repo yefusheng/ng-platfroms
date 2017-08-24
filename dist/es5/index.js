3b08468ed393d584cb7152ca78a7e67aa19dc11c
yefusheng <372758022@qq.com>
2017-08-24 17:53:40 +0800

    -----------

--- dist/es5/index.js
+++ dist/es5/index.js
@@ -15,6 +15,8 @@ var Storage_service_1 = require("./service/Storage.service");
 exports.StorageService = Storage_service_1.StorageService;
 var Logger_service_1 = require("./service/Logger.service");
 exports.LoggerService = Logger_service_1.LoggerService;
+var TransFile_service_1 = require("./service/TransFile.service");
+exports.TransFileService = TransFile_service_1.TransFileService;
 var ExePlatform_service_1 = require("./ExePlatform.service");
 exports.ExePlatformService = ExePlatform_service_1.ExePlatformService;
 //export {ExePlatformConponentModule} from './dynamic-component/ExePlatformConponentModule';/
@@ -24,3 +26,7 @@ var WechatPlatformModule_1 = require("./Wechat/WechatPlatformModule");
 exports.WechatPlatformModule = WechatPlatformModule_1.WechatPlatformModule;
 var NativePlatformModule_1 = require("./Native/NativePlatformModule");
 exports.NativePlatformModule = NativePlatformModule_1.NativePlatformModule;
+var NativeService_1 = require("./native/service/transfer/NativeService");
+exports.NativeService = NativeService_1.NativeService;
+var util_1 = require("./util/util");
+exports.log = util_1.log;
